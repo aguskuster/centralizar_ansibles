23116a5b8dafe855c4a18b5cb0be8e3ecc888951
root <root@kubemaster.syntech.intra>
2023-08-29 11:52:58 -0300

    Cambios ansible

--- roles/kubernetes-server/templates/Samba.sh
+++
@@ -1,6 +0,0 @@
-echo "[BkBases]" >> /etc/samba/smb.conf
-echo "comment = directorio compartido con samba" >> /etc/samba/smb.conf
-echo "path = /home/back/BkBases">> /etc/samba/smb.conf
-echo "valid users = back">> /etc/samba/smb.conf
-echo "public = no" >> /etc/samba/smb.conf
-echo "writable = yes" >> /etc/samba/smb.conf
